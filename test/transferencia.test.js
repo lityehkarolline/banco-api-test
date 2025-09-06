89ed2499399fe417bc1c3375f83cf7e2eed7654a
Lityeh <lityehkfs@gmail.com>
2025-09-05 21:56:42 -0300

    Adicionando o hook de beforeEach para captura do token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,9 +5,13 @@ const { obterToken } = require('../helpers/autenticacao')
 
 describe('Transferências', () => {
     describe('POST /transferencias', () => {
+        let token
+
+        beforeEach( async () => {
+            token = await obterToken('julio.lima', '123456')
+        })
+
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {
-        
-            const token = await obterToken('julio.lima', '123456')
                 
             const resposta = await request(process.env.BASE_URL)
                 .post('/Transferencias')
@@ -24,9 +28,7 @@ describe('Transferências', () => {
         })
 
         it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de R$ 10,00', async () => {
-            
-            const token = await obterToken('julio.lima', '123456')
-                
+
             const resposta = await request('http://localhost:3000')
                 .post('/Transferencias')
                 .set('Content-Type', 'application/json')
